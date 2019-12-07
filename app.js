63ee228a41c61156d29d17de8f806790cdb23e29
jleo (Berrios Leo <jleo@zegelipae.edu.pe>
2019-12-06 19:06:16 -0500

    listado de pedidos

---
+++ app.js
@@ -0,0 +1,107 @@
+
+const example1 = new Vue({
+    el: '#example-1',
+    data: {
+        items: [
+            {
+                id: '000001', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de pollo', 
+                ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: true,
+                drinkName: 'gaseosa',
+                pending: true,
+                sended: false
+            },
+            {
+                id: '000002', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de carne', ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: false,
+                drinkName: 'gaseosa',
+                pending: true,
+                sended: false
+            },
+            {
+                id: '000001', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de pollo', ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: false,
+                drinkName: 'gaseosa',
+                pending: false,
+                sended: true
+            },
+            {
+                id: '000001', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de pollo', ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: false,
+                drinkName: 'gaseosa',
+                pending: false,
+                sended: false
+            },{
+                id: '000001', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de pollo', ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: false,
+                drinkName: 'gaseosa',
+                pending: false,
+                sended: true
+            },
+            {
+                id: '000001', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de pollo', ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: false,
+                drinkName: 'gaseosa',
+                pending: false,
+                sended: false
+            },
+            {
+                id: '000001', time: '04/12/2018', hour: '10.10.10', client_name: 'Ale', product_name: 'hamburgesa de pollo', ingredients: [
+                    {
+                        name: 'tomate',
+                    },
+                    {
+                        name: 'cebolla',
+                    },
+                ],
+                drink: false,
+                drinkName: 'gaseosa',
+                pending: false,
+                sended: false
+            }
+   
+        ]
+    }
+})
